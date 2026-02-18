1f40d36d0a4be8bbbf34b78c71917f86b3efc40e
Anderson Fernandes <95195152+AndersonBF@users.noreply.github.com>
2026-02-18 11:25:41 -0300

    parte_principal_refeita4

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... outras configs
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Adicione esta linha para evitar que o build falhe por erros de renderização
   experimental: {
     missingSuspenseWithCSRBailout: false,
     serverActions: {
-      bodySizeLimit: '10mb', // <--- Isso é obrigatório para arquivos maiores
+      bodySizeLimit: '10mb',
     },
-  }
-
-  
+  },
 };
+
+module.exports = nextConfig;
